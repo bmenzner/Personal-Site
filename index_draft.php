4f0eaad9e57b20d4b962e321a641161941e4c50e
bmenzner <bmenzner@umich.edu>
2021-09-23 15:45:57 -0400

    initial commit

---
+++ index_draft.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Ben Menzner</title>
+    <link rel="stylesheet" href="css/index.css"/>
+  </head>
+
+
+<body>
+  <header>
+    <a href="#" class="logo">Ben Menzner</a>
+    <ul>
+      <li><a href="https://www.linkedin.com/in/ben-menzner-42a200196/">LinkedIn</a></li>
+    </ul>
+  </header>
+  <section class="banner">
+    <div class="outer">
+      <div class="box1">
+        <u> Dedicated </u>
+      </div>
+
+      <div class="box2">
+        <u> Creative </u>
+      </div>
+
+      <div class="box3">
+        <u> Ardent </u>
+      </div>
+    </div>
+
+  </section>
+  <script type="text/javascript">
+    window.addEventListener("scroll", function(){
+      var header = document.querySelector("header");
+      header.classList.toggle("sticky", window.scrollY > 0);
+    })
+  </script>
+  
+
+
+
+
+</body>
+
+</html>
